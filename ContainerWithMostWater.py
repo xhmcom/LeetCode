47fae034d703d673d46fcf70813a86e18eeccad5
xhm <xuhuanminzj@hotmail.com>
2017-09-05 15:05:05 +0800

    No.11 problem

---
+++ ContainerWithMostWater.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+# __author__ = 'xhm'
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        max_area = 0
+        i = 0
+        j = len(height) - 1
+        while i < j:
+            max_area = max(max_area, (j - i) * min(height[i], height[j]))
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+        return max_area
